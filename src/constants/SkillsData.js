af75c98004d1b1c7a343a7034908b64aa3166799
Mobina Karimi <mobina.karimi.mk87@gmail.com>
2025-04-26 08:53:32 +0000

    complete web wihout header responsive

--- src/constants/SkillsData.js
+++ src/constants/SkillsData.js
@@ -1,7 +1,14 @@
 import img1 from "../assets/HTML.png";
 import img2 from "../assets/CSS.png";
+import img3 from "../assets/scss.png";
+import img4 from "../assets/bootstrap.jpg";
 import img5 from "../assets/JavaScript.png";
 import img6 from "../assets/React.png";
+import img7 from "../assets/redux.png";
+import img8 from "../assets/responsive.png";
+import img9 from "../assets/git.png";
+import img10 from "../assets/materialUi.png";
+import img11 from "../assets/sql.png";
 
 const SkillsData = [
   {
@@ -16,12 +23,12 @@ const SkillsData = [
   },
   {
     id: "03",
-    image: img1,
+    image: img3,
     title: "SCSS",
   },
   {
     id: "04",
-    image: img1,
+    image: img4,
     title: "BOOTSTRAP",
   },
   {
@@ -36,27 +43,27 @@ const SkillsData = [
   },
   {
     id: "07",
-    image: img1,
+    image: img7,
     title: "REDUX",
   },
   {
     id: "08",
-    image: img1,
+    image: img8,
     title: "RESPONSIVE WEB DESIGN",
   },
   {
     id: "09",
-    image: img1,
+    image: img9,
     title: "GIT",
   },
   {
     id: "10",
-    image: img1,
+    image: img10,
     title: "MATERIAL UI",
   },
   {
     id: "11",
-    image: img1,
+    image: img11,
     title: "SQL SERVER",
   },
 ];
